384aa8c5e763246c03cb0f6cecbd10631536ff2d
Nathaly <nathalykuuingtelecom@gmail.com>
2024-11-11 11:27:37 -0300

    Actualización de archivos de configuración

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-BASE_URL = "https://cnt-88c4f145-821e-484c-9a8a-f9b174444e55.containerhub.tripleten-services.com"
+BASE_URL = "https://cnt-9afd7f41-2275-4fe9-8de2-cbbed401ecc5.containerhub.tripleten-services.com"
 CREATE_USER_URL = f"{BASE_URL}/api/v1/users"
 CREATE_KIT_URL = f"{BASE_URL}/api/v1/kits"
