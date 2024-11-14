d9f295ac0495a18a1c58d77caf636d0139c1d734
Nathaly <nathalykuuingtelecom@gmail.com>
2024-11-14 10:13:14 -0300

    Actualización de configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 import os
-BASE_URL = os.getenv("BASE_URL", "https://cnt-7bf1dd38-50b8-46ee-8b14-43fd133988f0.containerhub.tripleten-services.com")
+BASE_URL = os.getenv("BASE_URL", "https://cnt-752fe526-b7b2-4378-aa43-bce4348d87c8.containerhub.tripleten-services.com")
 CREATE_USER_URL = f"{BASE_URL}/api/v1/users"
 CREATE_KIT_URL = f"{BASE_URL}/api/v1/kits"
